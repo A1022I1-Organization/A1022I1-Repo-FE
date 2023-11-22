0bcfb12da6636a5f6e497ec7059fef983f70a16e
HoTanHuy <78059369+HoTanHuy@users.noreply.github.com>
2023-11-23 00:10:09 +0700

    huy

--- medical_management/src/layouts/List.jsx
+++ medical_management/src/layouts/List.jsx
@@ -5,25 +5,23 @@ import React, { useMemo, useState } from 'react'
 export default function List() {
   const [state, setState] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [page,setPage] = useState(6)
-  const [condition,setCondition] = useState("")
-  useMemo(async() => {
-    console.log(condition)
-    const value = await axios.get(`http://localhost:8080/api/supply/list?c=${condition}&p=${page}`);
-
-    setState (value.data);
-
-   
-  },[page,condition])
+  const [page,setPage] = useState(6);
+  const [condition,setCondition] = useState("");
+  const [nameSort,setNameSort] = useState(true);
+  const [priceSort,setPriceSort] = useState(true);
+  useMemo(async() => {  
+    console.log(`http://localhost:8080/api/supply/list?c=${condition}&p=${page}&ns=${nameSort?"asc":"desc"}&ps=${priceSort?"asc":"desc"}`);
+    const value = await axios.get(`http://localhost:8080/api/supply/list?c=${condition}&p=${page}&ns=${nameSort?"asc":"desc"}&ps=${priceSort?"asc":"desc"}`); 
+    setState (value.data); 
+  },[page,condition,nameSort,priceSort]);
   useMemo(async() => {
     const value = await axios.get(`http://localhost:8080/api/supply/getCategory`);
     setCategories (value.data); 
    
-  },[])
+  },[]); 
   return (
-    <div className="container">
-    <div className="row">
-        
+<div className="container" id='row21'>
+    <div className="row" >
         <nav id="sidebar21" className="col-md-3 col-lg-2 d-md-block bg-light sidebar px-2">
             <div className="position-sticky">
                 <h3>Sản phẩm</h3>       
@@ -38,9 +36,18 @@ export default function List() {
                             <div className="accordion-body">
                                 <ul className="nav flex-column" >
                                    
-                                    {categories.map(item =>   <li className="btn btn-white" id='nav21'>
-                                        <button className="btn btn-lightyellow" type="button" onClick={() => setCondition(item.id)}>{item.name}</button>
+                                    {categories.map(item =>   <li className="btn btn-lightyellow" id='nav21'>
+                                        <p onClick={() => setCondition(item.id)}>{item.name}</p>
                                     </li>)}
+                                    
+                                    {/* {categories.map(item => (
+                                            <li  id='nav21'
+                                            key={`nav${item.id}`}
+                                            className={`btn btn-white ${setCondition === item.id ? 'active' : ''}`}
+                                            >
+                                                <p onClick={() => setCondition(item.id)}>{item.name}</p>
+                                            </li>
+                                    ))} */}
                                 </ul>
                             </div>
                         </div>
@@ -52,25 +59,61 @@ export default function List() {
 
        
         <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div className="row mb-3">
+                <div className="col-md-6">
+                    
+                    <div className="input-group">
+                        <input type="text" className="form-control" placeholder="Tìm kiếm theo tên" id="searchInput"/>
+                        <button className="btn btn-outline-success" type="button" id="searchButton">Tìm kiếm</button>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    
+                    <div className="row">
+                        <div className="col-md-6">
+                            <label htmlFor="sortByName">Sắp xếp theo tên:</label>
+                            <select className="form-select" id="sortByName" onChange={() => setNameSort(!nameSort)}>
+                                <option value="az">A-Z</option>
+                                <option value="za">Z-A</option>
+                            </select>
+                        </div>
+                        <div className="col-md-6">
+                            <label htmlFor="sortByPrice">Sắp xếp theo giá:</label>
+                            <select className="form-select" id="sortByPrice" onChange={() => setPriceSort(!priceSort)}> 
+                                <option value="lowToHigh">Giá từ thấp đến cao</option>
+                                <option value="highToLow">Giá từ cao đến thấp</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="container" id="product-container">
                 <div className="row">
                     {state?
                         state.map(item =>  <div className="col-md-4">
                         <div className="card mb-4" id='cardmb4' style={{ width: '20rem' }}>
-                            <img src={item.picture} id='imgitem' className="card-img-top" alt="Ảnh 1" style={{ objectFit: 'cover' }} />
+                            <div id='div21'>
+                            <img src={item.picture} className="card-img-top" alt="Ảnh 1" />
+                            </div>
                             <div className="card-body" id='cardbody' style={{ height: '8rem' }}>
                                 <h5 className="card-title">{item.name}</h5>
                                 <p className="card-text">Giá: {item.price}</p>
                             </div>
                         </div>
-                    </div>) : "Hiện Tại Không có sản phẩm"
+                    </div>) : <h1 id='error21'>Hiện Tại Không có sản phẩm</h1> 
                     }
                 {
-                    page <= 6 ? <button class="btn btn-primary" onClick={() => setPage(prev => prev += 4)}> xem them ... </button> : 
+                    state ?
+                    page <= 6 ? 
                     <div>
-                         <button class="btn btn-secondary" onClick={() => setPage(prev => prev -= 4)}> rut gon ... </button>
-                         <button class="btn btn-primary" onClick={() => setPage(prev => prev += 4)}> xem them ... </button>
+                    <button class="btn btn-primary" onClick={() => setPage(prev => prev += 4)}> Xem Thêm ... </button>
+                    </div>
+                     : 
+                    <div>
+                         <button class="btn btn-secondary" onClick={() => setPage(prev => prev -= 4)}> Rút gọn ... </button>
+                         <button class="btn btn-primary" onClick={() => setPage(prev => prev += 4)}> Xem Thêm ... </button>
                         </div>
+                    : <div></div>
                 }
                 </div>
             </div>
